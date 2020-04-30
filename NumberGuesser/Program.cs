3b4705856aefd6c87029f52df7b5610504ffd3bc
Vidur Bhardwaj <vidurbhardwaj96@gmail.com>
2020-04-30 12:37:49 +0530

    .net_number_guesser

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -16,24 +16,37 @@ namespace NumberGuesser
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("{0}: version {1} by {2}", appName, appVersion, appAuthor);
             Console.WriteLine("Please enter your name : ");
-            string inputName= Console.ReadLine();
+            string inputName = Console.ReadLine();
             Console.WriteLine("Hi {0} let's play a game", inputName);
             Console.WriteLine("Guess any number between 1 and 10 : ");
-            int guess = 0;
-            int correctNumber = 7;
-            while (guess != correctNumber)
+            while (true)
             {
-                String input = Console.ReadLine();
-                guess = Int32.Parse(input);
-                if(guess!=correctNumber)
+                int guess = 0;
+                Random random = new Random();
+                int correctNumber = random.Next(1, 10);
+
+                while (guess != correctNumber)
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Wrong Number :(, Please try again");
-                    Console.ResetColor();
+                    String input = Console.ReadLine();
+                    if (!int.TryParse(input, out guess))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Please Enter Integer only !");
+                        Console.ResetColor();
+                        continue;
+                    }
+                    guess = Int32.Parse(input);
+                    if (guess != correctNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Wrong Number :(, Please try again");
+                        Console.ResetColor();
+                    }
                 }
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Correct guess :) ....Victory!");
+                Console.WriteLine("Please try playing again : ");
             }
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Correct guess :) ....Victory!");
         }
     }
 }
